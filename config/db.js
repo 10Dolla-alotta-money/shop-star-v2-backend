bf4fc1f3a9f650df70b1da78aede592f8e987a26
Zarman-oss <zarmanimran@gmail.com>
2024-02-07 13:11:16 +0500

    .

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`MongoDB Connected : ${conn.connection.host}`)
-    } catch (error) {
-        console.log(`Error: ${error.message}`)
-        process.exit(1);
-    }
-
-}
-export default connectDB; 
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected : ${conn.connection.host}`.yellow);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+export default connectDB;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import express from 'express';
+import colors from 'colors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
@@ -23,21 +24,16 @@ app.use(cookieParser());
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('/api/upload', uploadRoutes);  
-
-
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
 if (process.env.NODE_ENV === 'production') {
-
   const __dirname = path.resolve();
 
   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
-
 
   app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
@@ -45,26 +41,24 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
   );
 } else {
+  app.get('/', (req, res) => res.send('Server is up'));
 
-  app.get('/', (req, res) => res.send('Server is up')); 
-  
-  // const __dirname = path.resolve();
+  const __dirname = path.resolve();
 
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-  // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
-  // app.get('/', (req, res) => {
-  //   res.send('API is running....');
-  // });
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
 }
 
 app.use(notFound);
 app.use(errorHandler);
 
-
 app.listen(port, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${port}`.cyan
+  )
 );
- 
 
 export default app;
